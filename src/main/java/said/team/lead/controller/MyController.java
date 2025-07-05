b0e0226d7fc2170cd2cf49977f13f7075108c86a
said mudarov <saidmudarov@mail.ru>
2025-07-05 13:58:20 +0500

    xml upgraded and added some claesses

---
+++ src/main/java/said/team/lead/controller/MyController.java
@@ -0,0 +1,25 @@
+package said.team.lead.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import said.team.lead.dao.EmployeeDAO;
+import said.team.lead.entity.Employee;
+
+import java.util.List;
+
+@Controller
+public class MyController {
+
+    @Autowired
+    private EmployeeDAO employeeDAO;
+
+    @RequestMapping("/")
+    public String showAllEmployees(Model model){
+
+        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        model.addAttribute("allEmps", allEmployees);
+        return "all-Employees";
+    }
+}
---
+++ src/main/java/said/team/lead/dao/EmployeeDAO.java
@@ -0,0 +1,9 @@
+package said.team.lead.dao;
+
+import said.team.lead.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeDAO {
+    public List<Employee> getAllEmployees();
+}
